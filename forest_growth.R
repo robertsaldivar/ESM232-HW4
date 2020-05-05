3af7d6f2c28e407ace021c602f6830e1191efe1d
robertsaldivar <rnsaldivar@bren.ucsb.edu>
2020-05-04 21:50:07 -0700

    started the forest growth function for hw4. I think that I have all of the variables that will be needed.

---
+++ forest_growth.R
@@ -0,0 +1,11 @@
+#' Dynamic model of forest growth
+#' 
+#' 
+#' 
+#' @author Robert Saldivar
+#' @author Madeline Gorchels
+#' 
+
+forest_growth = function(initialC, r, g, carry_capacity, air_temp, dt, dc) {
+  
+}
