77de0377f0d36207fd94d5720380634c744eb0a9
gorchels <gorchels@bren.ucsb.edu>
2020-05-08 11:58:50 -0700

    Working on the function

--- forest_growth.R
+++ forest_growth.R
@@ -7,5 +7,12 @@
 #' 
 
 forest_growth = function(initialC, r, g, carry_capacity, air_temp, dt, dc) {
-  
+  if(C <= canopy_closure)
+    
+  if(C >= canopy_closure)
+    dt=g
+  if(air_temp <= 0)
+    dt=0
+  if(C=carry_capacity)
+     dt=0
 }
