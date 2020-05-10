c9f08f41ac917eec78957b18814c055fb42ac6a6
gorchels <gorchels@bren.ucsb.edu>
2020-05-10 16:17:31 -0700

    Updated function and Rmarkdown

--- forest_growth.R
+++ forest_growth.R
@@ -15,10 +15,10 @@ forest_growth = function(initialC, r, g, carry_capacity, air_temp, dC_dt) {
    else{
      dC_dt=g
    }
-  #if(air_temp <= 0) {
-   # dC_dt=0}
+   #if(air_temp <= 0) {
+   #dC_dt=0}
   #if(C=carry_capacity) {
-     #dC_dt=0}
+  # dC_dt=0}
   
   return(C)
    }
