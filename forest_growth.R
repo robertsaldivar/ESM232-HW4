9eded5569e1836b92e083822fa64a34cb4c12762
gorchels <gorchels@bren.ucsb.edu>
2020-05-11 08:33:43 -0700

    Function isn't quiet finished but I got the oDE solver to run with it

--- forest_growth.R
+++ forest_growth.R
@@ -4,20 +4,20 @@
 #' 
 #' @author Robert Saldivar
 #' @author Madeline Gorchels
-#' 
+#' r, g, c_closure, carry_capacity, air_temp
 
-forest_growth = function(time, initialC, r, g, c_closure, carry_capacity, air_temp) {
+forest_growth = function(time, initialC, parms) {
   
    C = initialC*dC_dt+initialC
   
-  if(C <= c_closure) {
-    dC_dt=initialC*r}
+  if(C <= parms$c_closure) {
+    dC_dt=initialC*parms$r}
    else{
-     dC_dt=g
+     dC_dt=parms$g
    }
-  # if (air_temp <= 0) {
+  # if (parms$air_temp <= 0) {
   # dC_dt=0}
-  #if (C=carry_capacity) {
+  #if (C=parms$carry_capacity) {
   # dC_dt=0}
   
   return(list(C))
