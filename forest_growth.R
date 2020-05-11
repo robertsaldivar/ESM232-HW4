21b2ea990e08c252cea8f937d235cbbbc631363e
gorchels <gorchels@bren.ucsb.edu>
2020-05-10 20:27:35 -0700

    Updated function

--- forest_growth.R
+++ forest_growth.R
@@ -6,7 +6,7 @@
 #' @author Madeline Gorchels
 #' 
 
-forest_growth = function(initialC, r, g, carry_capacity, air_temp, dC_dt) {
+forest_growth = function(initialC, r, g, carry_capacity, air_temp, canopy_closure) {
   
    C = initialC*dC_dt+initialC
   
@@ -15,9 +15,9 @@ forest_growth = function(initialC, r, g, carry_capacity, air_temp, dC_dt) {
    else{
      dC_dt=g
    }
-   #if(air_temp <= 0) {
-   #dC_dt=0}
-  #if(C=carry_capacity) {
+  # if (air_temp <= 0) {
+  # dC_dt=0}
+  #if (C=carry_capacity) {
   # dC_dt=0}
   
   return(C)
